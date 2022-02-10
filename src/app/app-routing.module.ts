a3b3e7563e30a88919832c04e1ac134a610c7e2e
jainam01 <shahjainam01921a@gmail.com>
2022-02-10 12:10:35 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CONTACTUSComponent } from './pages/contact-us/contact-us.component';
+import { DonationComponent } from './pages/donation/donation.component';
+import { FAQComponent } from './pages/faq/faq.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/signup/login/login.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'donation',component:DonationComponent},
+  {path:'faq',component:FAQComponent},
+  {path:'contact-us',component:CONTACTUSComponent},
+  {path:'signup',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './sharepage/navigation/navigation.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DonationComponent } from './pages/donation/donation.component';
+import { FAQComponent } from './pages/faq/faq.component';
+import { CONTACTUSComponent } from './pages/contact-us/contact-us.component';
+import { LoginComponent } from './pages/signup/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent,
+    DonationComponent,
+    FAQComponent,
+    CONTACTUSComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
